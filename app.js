423a07c4ad978fc738e5decb2a191e76d1315b39
Danny Simms <djfsimms@hotmail.com>
2023-03-19 19:58:50 -0230

    Remove comments

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-// Your calculator is going to contain functions for all of the basic math operators you 
-// typically find on simple calculators, so start by creating functions for the following 
-// items and testing them in your browser’s console.
-// add
-// subtract
-// multiply
-// divide
-// Create a new function operate that takes an operator and 2 numbers and then calls one of 
-// the above functions on the numbers.
-
-// Calculator function(operator, num1, num2)
-// Add function(num1, num2) and returns sum
-// Subtract function(num1, num2) and returns difference
-// Multiphy function(num1, num2) and returns product
-// Divide function(num1, num2) and returns quotient
-// Operate function(operator, num1, num2) and returns the result based on the operator provided
 
 function calculator(num1, num2, operator) {
     const add = (num1, num2) => {return num1 + num2}
@@ -37,12 +21,6 @@ function calculator(num1, num2, operator) {
         return total;   
     }
 };
-
-// Listen to each one of the number and decimal key for clicks
-// Upon click add the value to the user number and display the number on the calc screen
-// Disable the decimal key after first click until a none number key is clicked
-
-// Reset calc should clear display, num values, reset decimal
 
 const numKeys = document.querySelectorAll(".digit");
 let userNum = "";
